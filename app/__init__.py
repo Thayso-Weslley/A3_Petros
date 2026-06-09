ee0d81b5d07f80ea26af4c78b156b4215cf5ed77
thiaventura <thi.ventura12@hotmail.com>
2026-06-08 22:53:26 -0300

    Plug das rotas

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,20 @@ def create_app():
     
     # Importação tardia e registro do Blueprint
     from app.api.lei_de_Newton_routes import lei_de_Newton_bp
+    from app.api.atrito_routes import atrito_bp
+    from app.api.energia_cinetica_routes import energia_cinetica_bp
+    from app.api.momento_routes import momento_bp
+    from app.api.peso_routes import peso_bp
+    from app.api.potencia_routes import potencia_bp
+    from app.api.quantidade_de_movimento_routes import quantidade_de_movimento_bp
     from app.api.repositorio_base_routes import repositorio_base_bp
     app.register_blueprint(lei_de_Newton_bp)
+    app.register_blueprint(atrito_bp)
+    app.register_blueprint(energia_cinetica_bp)
+    app.register_blueprint(momento_bp)
+    app.register_blueprint(peso_bp)
+    app.register_blueprint(potencia_bp)
+    app.register_blueprint(quantidade_de_movimento_bp)
     app.register_blueprint(repositorio_base_bp)
 
     return app
