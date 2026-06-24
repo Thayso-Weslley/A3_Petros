fbd778502f0905ca554df1884bc92887f44e7bd1
thayso <thayso.weslley@gmail.com>
2026-06-23 21:32:16 -0300

    back-up da versão antiga

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -14,6 +14,7 @@ import { atritoUI } from './UI/atritoUI.js';
 import { formula_de_NavierUI } from './UI/formula_de_NavierUI.js';
 import { equacao_de_EulerUI } from './UI/equacao_de_EulerUI.js';
 
+// old version
 document.addEventListener('DOMContentLoaded', () => {
     // 1. MAPEAMENTO DE DOM
     const DOM = {
