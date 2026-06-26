8843963c7edf7180b04c14d154a036e6df0b02af
thayso <thayso.weslley@gmail.com>
2026-06-26 00:18:54 -0300

    arquivo de Barrel FIles - Centralizador de imports

---
+++ app/static/js/UI/componentes/index.js
@@ -0,0 +1,12 @@
+// UI/componentes/index.js
+
+export { lei_de_NewtonUI } from './lei_de_NewtonUI.js';
+export { energia_cineticaUI } from './energia_cineticaUI.js';
+export { pesoUI } from './pesoUI.js';
+export { momentoUI } from './momentoUI.js';
+export { potenciaUI } from './potenciaUI.js';
+export { quantidade_de_movimentoUI } from './quantidade_de_movimentoUI.js';
+export { trabalhoUI } from './trabalhoUI.js';
+export { atritoUI } from './atritoUI.js';
+export { formula_de_NavierUI } from './formula_de_NavierUI.js';
+export { equacao_de_EulerUI } from './equacao_de_EulerUI.js';
